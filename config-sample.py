9bb2d8229f8ea61f6a0c1cfefddcb322bd5692d8
Ryan <81808867+ryu878@users.noreply.github.com>
2023-02-06 18:33:03 +0300

    Update config-sample.py

--- config-sample.py
+++ config-sample.py
@@ -1,16 +1,16 @@
 # Bybit settings
 domain      = 'bybit'
 endpoint    = 'https://api.bybit.com'
-api_key     = ''
-api_secret  = ''
+api_key     = ''                      # paste your API key here
+api_secret  = ''                      # paste your API secret here
 
 # Trading settings
-symbol = 'BTCUSD'
-csize = 1
-min_fee = 0.17
-divider = 7
+symbol = 'BTCUSD'                     # asset to trade. Never tested on another asset.
+csize = 1                             # lot size
+min_fee = 0.17                        # min distance to take profit
+divider = 7                           # deleverage divider
 
-# Database settigns
+# Database settings
 # redis_host = '192.168.1.4'
 redis_host = 'localhost'
 redis_port = 6379
